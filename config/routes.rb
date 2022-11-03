28c60aef1baaf5a591246138796d7d93410573f9
farhan <farhanj.mohamed@gmail.com>
2022-11-03 15:00:14 -0700

    added routes and seeded data

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get "/photos" => "photos#index"
+  get "/photos/:id" => "photos#show"
+  post "/photos" => "photos#create"
+  patch "/photos/:id" => "photos#update"
+  delete "/photos/:id" => "photos#destroy"
 end
